ef1389ce8233ec4da2ebfd96e4197761225ed041
prasad00019 <93087755+prasad00019@users.noreply.github.com>
2022-10-31 23:33:00 +0530

    Update BridgeContract.sol
    updated Bridge contract

--- contract/BridgeContract.sol
+++ contract/BridgeContract.sol
@@ -1,71 +1,42 @@
 pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract bridgeCustody is IERC721Receiver, ReentrancyGuard, Ownable {
 
-  uint256 public costNative = 0.000075 ether;
+  event BridgeNFT(address from , uint Id , string uri); 
+  address private Owner ; 
+  ERC721 private nft;
 
-  struct Custody {
-    uint256 tokenId;
-    address holder;
+
+  modifier onlyOwner {
+    require(msg.sender == owner); 
+    _; 
   }
 
-  mapping(uint256 => Custody) public holdCustody;
-
-  event NFTCustody (
-    uint256 indexed tokenId,
-    address holder
-  );
-
-
-  ERC721Enumerable nft;
-
-   constructor(ERC721Enumerable _nft) {
+   constructor(ERC721 _nft) {
+    Owner = msg.sender; 
     nft = _nft;
   }
+  // this is the function that will be called while transferring the Tokens where 
+  // the NFT will be burned by emiting an event from that even we are to mint another NFT
+  // in a destinated chain with same Token iD 
 
-  function retainNFTN(uint256 tokenId) public {
-      require(msg.value == costNative, "Not enough balance to complete transaction.");
-      require(nft.ownerOf(tokenId) == msg.sender, "NFT not yours");
-      require(holdCustody[tokenId].tokenId == 0, "NFT already stored");
-      holdCustody[tokenId] =  Custody(tokenId, msg.sender);
-      nft.transferFrom(msg.sender, address(0), tokenId);
-      emit NFTCustody(tokenId, msg.sender);
+  function BridgeToken(uint Token_Id) external nonReentrant{
+    require(nft.ownerOf(Token_Id) == msg.sender, "you are not the owner of this NFT");
+    string memory uri = nft.tokenURI(Token_Id); 
+    nft._burn(Token_Id); 
+    emit BridgeNFT(msg.sender,Token_Id,uri); 
   }
 
-  function retainNew(uint256 tokenId) public nonReentrant {
-      require(holdCustody[tokenId].tokenId == 0, "NFT already stored");
-      holdCustody[tokenId] =  Custody(tokenId, msg.sender);
-      nft.transferFrom(msg.sender, address(this), tokenId);
-      emit NFTCustody(tokenId, msg.sender);
+  function GetTokenAddress() external view onlyOwner returns(address){
+    return address(nft); 
   }
 
- function updateOwner(uint256 tokenId, address newHolder) public nonReentrant  {
-   holdCustody[tokenId] =  Custody(tokenId, newHolder);
-   emit NFTCustody(tokenId, newHolder);
- }
- 
- function releaseNFT(uint256 tokenId, address wallet) public nonReentrant  {
-      nft.transferFrom(address(this), wallet, tokenId);
-      delete holdCustody[tokenId];
- }
-
-  function emergencyDelete(uint256 tokenId) public nonReentrant  {
-      delete holdCustody[tokenId];
- }
-
-  function onERC721Received(
-        address,
-        address from,
-        uint256,
-        bytes calldata
-    ) external pure override returns (bytes4) {
-      require(from == address(0x0), "Cannot Receive NFTs Directly");
-      return IERC721Receiver.onERC721Received.selector;
-    }
-
+  function SetTokenAddress(ERC721 _nft) external onlyOwner {
+    nft = _nft ; 
+  }
   
 }
